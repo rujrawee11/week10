dacf8cb3cf06b35c51bd231cc53c77647eed9c32
rujrawee meela <73680402+rujrawee11@users.noreply.github.com>
2023-04-03 21:14:13 +0700

    Update index.js

--- FE/pages/index.js
+++ FE/pages/index.js
@@ -1,136 +1,118 @@
-import axios from "axios";
-import React, { useState } from "react";
-import "./App.css";
+import axios from 'axios'
+import Head from 'next/head'
+import Image from 'next/image'
+import react, { useState } from 'react'
+import { headers } from '../next.config'
+import styles from '../styles/Home.module.css'
 
-function App() {
-  const hostname = `${window.location.hostname}`;
-  console.log(hostname);
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [numbers, setNumbers] = useState([]);
-  const [images, setImage] = useState(null);
-  const [showImage, setShowImage] = useState(null);
-  const [data, setData] = useState(null);
-  const handleChange = (event) => {
-    let input = document.getElementById("input");
-    var fReader = new FileReader();
-    fReader.readAsDataURL(input.files[0]);
+export default function Home({ host }) {
+  const hostname = host.substring(0, host.indexOf(":"))
+  const [name, setName] = useState('')
+  const [surname, setSurname] = useState('')
+  const [num, setNum] = useState([])
+  const [image, setImage] = useState(null)
+  const [Images, setImages] = useState(null)
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const chooseFiles = (event) => {
+    let input = document.getElementById('input')
+    var fReader = new FileReader()
+    fReader.readAsDataURL(input.files[0])
     fReader.onloadend = (e) => {
-      console.log(e.target.result);
-      setImage(e.target.result);
-    };
-  };
-  const submitPost = async () => {
-    let listNumbers = numbers.split(" ");
-    console.log("check");
-    console.log(`${hostname}:8088/process-image`);
-    try {
-      console.log("submit");
-      console.log(images);
-      console.log(name);
-      console.log(surname);
-      const result = await axios.post(
-        `http://${hostname}:8088/process-image`,
-        { image: images, name: name, surname: surname, numbers: listNumbers },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      setData(result.data);
-      setShowImage(result.data.processed_image);
-    } catch (e) {
-      console.log(e);
+      console.log(e.target.result)
+      setImage(e.target.result)
     }
-  };
+  }
+  const submitData = async () => {
+    let sendNumber = num.split(" ")
+    try {
+      setLoading(true)
+      const result = await axios.post(`http://${hostname}:8088/process-image`, { image: image, name: name, surname: surname, num: sendNumber }, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      setData(result.data)
+      setImages(result.data.processed_image)
+      setLoading(false)
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
   return (
-    <div className="container">
-      <div className="row mt-4">
-        <div className="col-12 col-md-6 offset-md-3">
-          <h2 className="my-4 text-center">Software Devtool</h2>
+    <div className={styles.container}>
+      <h1 style={{ padding: "2%" }}>LAB Week 9</h1>
+      <div style={{ display: "column", justifyContent: "center", alignItems: "center" }}>
+        <div style={{
+          display: "column"
+        }}>
 
-          
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <div className="form-group">
-                <label htmlFor="name">Your Name</label>
-                <input
-                  onChange={(e) => setName(e.target.value)}
-                  type="text"
-                  className="form-control"
-                  id="name"
-                  placeholder="Enter your name"
-                />
-              </div>
-              <div className="form-group" style={{ marginLeft: 10 }}>
-                <label htmlFor="surname">Your Surname</label>
-                <input
-                  onChange={(e) => setSurname(e.target.value)}
-                  type="text"
-                  className="form-control"
-                  id="surname"
-                  placeholder="Enter your surname"
-                />
-              </div>
-              <div className="form-group" style={{ marginLeft: 10 }}>
-                <label htmlFor="number">Your Susent ID</label>
-                <input
-                  onChange={(e) => setNumbers(e.target.value)}
-                  type="text"
-                  className="form-control"
-                  id="number"
-                  placeholder="Enter your student id"
-                />
-              </div>
-            </div>
-            <div className="form-group">
-              <input id="input" type="file" onChange={() => handleChange()} />
-            </div>
-          <button type="submit" className="btn btn-primary" onClick={() => submitPost()}>Submit</button>
-          
+          <div>Choose File :</div>
+          <input id='input' type='file' onChange={() => chooseFiles()} />
         </div>
+        <div style={{
+          display: "column"
+        }}>
 
-        {images && (
-          <div
-            style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-          >
-            <img
-              id="images"
-              width={500}
-              height={350}
-              src={images}
-              alt="img1"
-              style={{ objectFit: "contain" }}
-            />
-          </div>
+          <div>Name :</div>
+          <input type={"text"} onChange={(e) => setName(e.target.value)} placeholder={"Enter Name"} />
+
+        </div>
+        <div style={{
+          display: "column"
+        }}>
+
+          <div>Surname :</div>
+          <input type={"text"} onChange={(e) => setSurname(e.target.value)} placeholder={"Enter Surname"} />
+        </div>
+        <div style={{
+          display: "column"
+        }}>
+
+          <div>Student ID: :</div>
+          <input type={"text"} onChange={(e) => setNum(e.target.value)} placeholder={"Enter Student ID"} />
+        </div>
+        <button onClick={() => submitData()}>Submit</button>
+      </div>
+      {image && (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Image id='image' width={600} height={400} src={image} alt='eiei' style={{ objectFit: 'contain' }} />
+        </div>
+      )}
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+        {Images && (
+          <Image id='image2' width={600} height={400} src={Images} alt='hello' style={{ objectFit: 'contain' }} />
         )}
-        <div
-          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-        >
-          {showImage && (
-            <img
-              id="image2"
-              width={500}
-              height={350}
-              src={showImage}
-              alt="imgoutput"
-              style={{ objectFit: "contain" }}
-            />
-          )}
-        </div>
-        {data && (
+      </div>
+      {
+        data && (
           <div>
-            <div>{data.name + " " + data.surname}</div>
+            <div>
+              {data.name + " " + data.surname}
+            </div>
             <ul>
-              {data.numbers.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
+              {
+                data.num.map((item, index) => (
+                  <li key={index}>
+                    {item}
+                  </li>
+                ))
+              }
             </ul>
           </div>
         )}
-      </div>
+      {
+        loading && (
+          <div style={{ position: "absolute", width: "100%", height: "100%", backgroundColor: "rgba(0,0,0, 0.5)", top: 0, left: 0 }}>
+            <div style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", fontSize: "50px" }}>
+              Loading
+            </div>
+          </div>
+        )
+      }
     </div>
-  );
+  )
 }
 
-export default App;
+export const getServerSideProps = async context => ({ props: { host: context.req.headers.host || null } })
